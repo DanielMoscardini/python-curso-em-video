f2a432a5271696920a98d803c488be2b1d85b7f2
DanielMoscardini <moscardinibdaniel@gmail.com>
2021-01-08 17:22:17 -0300

    Adicionando ex086.py

--- Curso Em Video Python/Mundo 3/ex086.py
+++ Curso Em Video Python/Mundo 3/ex086.py
@@ -7,3 +7,12 @@ Ex:
 [4] [5] [6]
 [7] [8] [9]
 """
+matriz = [[0, 0, 0], [0, 0, 0, ], [0, 0, 0]]
+for l in range(0,3):
+    for c in range(0, 3):
+        matriz[l][c] = int(input(f'Digite um valor para [{l}, {c}]: '))
+print('-=' * 30)
+for l in range(0, 3):
+    for c in range(0, 3):
+        print(f'[{matriz[l][c]:^5}]',end='') #formata em 5 casa decimais, centralizadas
+    print() #Identação para quebrar uma linha assim que completar o for
