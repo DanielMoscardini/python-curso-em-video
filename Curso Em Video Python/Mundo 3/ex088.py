a0e5cbdfac2497c9db7b520cf11040ef6c325881
Daniel Moscardini <moscardinibdaniel@gmail.com>
2021-01-11 13:42:31 -0300

    Atualizando exercício 88

--- Curso Em Video Python/Mundo 3/ex088.py
+++ Curso Em Video Python/Mundo 3/ex088.py
@@ -9,3 +9,13 @@ Jogo 2: [14, 20, 23, 35, 38, 47]
 Jogo 3: [2, 11, 15, 24, 43, 48]
 Jogo 4: [6, 26, 28, 30, 46]
 """
+from random import sample
+from time import sleep
+
+jogos = list()
+n = int(input('Quantos jogos?: '))
+for c in range(n):
+    a = sorted(sample(range(1, 61), 6))
+    jogos.append(a[:])
+    print(f'Jogo {c + 1}: {a}')
+    sleep(0.5)
