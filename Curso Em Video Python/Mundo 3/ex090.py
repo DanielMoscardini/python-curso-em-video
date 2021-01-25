d3c5594018787d2cc8c4540c8212a2417a0a83e2
Daniel Moscardini <moscardinibdaniel@gmail.com>
2021-01-25 19:29:06 -0300

    Adicionando ex090

--- Curso Em Video Python/Mundo 3/ex090.py
+++ Curso Em Video Python/Mundo 3/ex090.py
@@ -1,5 +1,18 @@
 """
 Faça um programa que leia nome e media de um aluno, guardando também a situação em um dicionário.
 No final, mostre o conteudo da estrutura na tela.
-Media < 6, situação = 'RECUPERAÇÃO'
+Media < 7, situação = 'RECUPERAÇÃO'
+Media < 5, situação = 'REPROVADO'
 """
+aluno = {}
+aluno['nome'] = str(input('Nome: '))
+aluno['media'] = float(input(f'Média de {aluno["nome"]}: '))
+if aluno["media"] >= 7:
+    aluno['situacao'] = 'Aprovado'
+elif 5 <= aluno["media"] < 7:
+    aluno['situacao'] = 'Recuperação'
+else:
+    aluno['situacao'] = 'Reprovado'
+print('-=' * 30)
+for k, v in aluno.items():
+    print(f' - {k} é igual a {v}')
