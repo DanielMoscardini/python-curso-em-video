3baf7a50349c579a0e39b1c8ce7d60389c697bdf
DanielMoscardini <moscardinibdaniel@gmail.com>
2021-02-01 21:59:04 -0300

    exercicios

--- Curso Em Video Python/Mundo 3/ex093.py
+++ Curso Em Video Python/Mundo 3/ex093.py
@@ -9,8 +9,8 @@ jogador = {}
 jogador['nome'] = str(input(f'Nome do Jogador: '))
 partidas = int(input(f'Quantas partidas {jogador["nome"]} jogou? '))
 for c in range(1, partidas + 1):
-	jogador_gols.append(int(input(f'Quantos gols na {c}º partida? ')))
-	soma = sum(jogador_gols)
+    jogador_gols.append(int(input(f'Quantos gols na {c}º partida? ')))
+    soma = sum(jogador_gols)
 
 jogador['gols'] = jogador_gols
 jogador['total'] = soma
@@ -23,13 +23,13 @@ print('-=' * 30)
 # 2º print:
 print('-=' * 30)
 for k, v in jogador.items():
-	print(f'O campo {k} tem valor {v}')
+    print(f'O campo {k} tem valor {v}')
 print('-=' * 30)
 
 # 3º print:
 print('-=' * 30)
 print(f'O jogador {jogador["nome"]} jogou {partidas} partidas')
 for i, v in enumerate(jogador['gols']):
-	print(f'	Na partida {i + 1}, fez {v} gols')
+    print(f'	Na partida {i + 1}, fez {v} gols')
 print(f'Foi um total de {soma} gols')
 print('-=' * 30)
--- Curso Em Video Python/Mundo 3/ex094.py
+++ Curso Em Video Python/Mundo 3/ex094.py
@@ -1,29 +1,35 @@
-"""
-Crie um programa que leia nome, sexo e idade de várias pessoas, guardando os dados de cada pessoa em um
-dicionário e todos os dicionários em uma lista. No final, mostre:
-A) Quantas pessoas foram cadastradas;
-B) A média de idade do grupo;
-C) Uma lista com todas as mulheres;
-D) Uma lista com todas as pessoas com idade acime da média;
-"""
-
-nome = []
-sexo = []
-idade = []
-
+galera = list()
+listam = list()
+dados = dict()
+soma = media = 0
 while True:
-	nome.append(str(input('Nome: ')))
-	sexo.append(str(input('M/F :  ')))
-	idade.append(int(input('Idade: ')))
-	resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
-	if resp in 'N':
+	dados['nome'] = str(input('Nome: '))
+	dados['sexo'] = ' '
+	while dados['sexo'] not in 'MF':
+		dados['sexo'] = str(input('Sexo: [M/F]')).upper().strip()[0]
+		if dados['sexo'] != 'M' and dados['sexo'] != 'F':
+			print('ERRO!. Digite Apenas M ou F.')
+		if dados['sexo'] == 'F':
+			listam.append(dados['nome'])
+	dados['idade'] = int(input('Idade: '))
+	soma += dados['idade']
+	galera.append(dados.copy())
+	resp = ' '
+	while resp not in 'SN':
+		resp = str(input('Deseja continuar: ')).upper().strip()[0]
+		if resp != 'S' and resp != 'N':
+			print('ERRO!. Digite Apenas S ou N.')
+	if resp == 'N':
 		break
-	elif resp not in 'S':
-		print(f'ERRO! Responda apenas S ou N.')
-		resp = str(input('Quer continuar? [S/N]: ')).strip().upper()[0]
-		if resp in 'N':
-			break
-
-print(f'Nome: {nome}\n'
-	  f'Sexo: {sexo}\n'
-	  f'Idade: {idade}')
+print('=-' * 30)
+print(f'Ao todos temos {len(galera)} pessoas cadastradas.')
+print(f'A média de idade é de {soma / len(galera):.2f}')
+media = soma / len(galera)
+if len(listam) >= 1:
+	print(f'As mulheres cadastradas foram: {listam}.')
+else:
+	print('Não foram cadastradas mulheres na lista.')
+print('Lista de pessoas acima da média:')
+for i, p in enumerate(galera):
+	if galera[i]['idade'] >= media:
+		print(p)
