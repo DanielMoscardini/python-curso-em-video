8c4808c8ee509dd4161be95c831fefb50b1ace91
DanielMoscardini <moscardinibdaniel@gmail.com>
2021-02-02 21:45:25 -0300

    Adicionando exercicios e aula 20

--- Curso Em Video Python/Mundo 3/Aula 20.py
+++ Curso Em Video Python/Mundo 3/Aula 20.py
@@ -1,9 +1,35 @@
-def pula_linha(mensagem):
-	print('-' * 30)
-	print(mensagem)
-	print('-' * 30)
+"""
+def soma(a, b):
+	print(f'a = {a} --- b = {b}')
+	print(f'A soma de {a} + {b} é de: {a + b}')
+	print()
 
 
-pula_linha('Curso de Python')
-pula_linha('Curso em Vídeo')
-pula_linha('Gustavo Guanabara')
+soma(4, 5)
+soma(8, 9)
+soma(2, 1)
+
+---------------------------------------------------------------------------------------------------------------------
+
+def contador(*num):  # parâmetro recebe diversos valores
+	print(num)
+
+
+contador(2, 1, 7)
+contador(8, 0)
+contador(4, 4, 7, 6)
+
+---------------------------------------------------------------------------------------------------------------------
+
+#Dobrando valores de uma lista
+def dobra_valores(list):
+	pos = 0
+	while pos < len(list):
+		list[pos] *= 2
+		pos += 1
+
+
+valores = [7, 2, 5, 0, 4]
+dobra_valores(valores)
+print(valores)
+"""
---
+++ Curso Em Video Python/Mundo 3/ex096.py
@@ -0,0 +1,4 @@
+"""
+Faça um programa que tenha uma função chamada area(), que calcule as dimensões de um terreno
+retangular (largura e comprimento) e mostre a área do terreno
+"""
